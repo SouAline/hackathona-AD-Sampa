efeac19314a3fa703da451ec5609aa60ce5601bb
Aline Souza <ds.alinesouza@gmail.com>
2022-03-22 10:01:42 -0300

    cria projeto com router

---
+++ shopping/src/components/Shopping.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{ name_store || "My Store"}}</h1>
+        <h2 v-html="subTitle"></h2>
+        <button
+        class="btn btn-primary"
+        @click="insert"  ></button>
+
+        <script>
+            export default{
+                name: "Shopping",
+                props: {
+                    name_store: String,
+                },
+                data(){
+                    return{
+                        subTitle: "<strong> Mult</strong> Shopping",
+                        insert: false,
+                        newItem:{
+                            product: "",
+                            priority: "low",
+                            color: "black",
+                            gift: "false",
+                            complements:[],
+                        },
+                        items: [
+                            {id: 1, price: 60,00, img: "https://, product: "chapéu""},
+                            {id: 1, price: 60,00, img: "https://, product: "chapéu""},
+                            {id: 1, price: 60,00, img: "https://, product: "chapéu""}
+                        ]
+                    }
+                }
+            }
+        </script>
+</template>
